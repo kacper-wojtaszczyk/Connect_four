64539b6391a569ba42ea2b09073169c379a8b8d3
Krystian Plich <krypli94@gmail.com>
2020-06-25 18:52:47 +0200

    Add restart and start screen

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
 import pygame.freetype
 import sys
-# import numpy as np
-# from os import path
-from grid import Grid
+from board import Grid
 from agent import Agent
+from gui import Button, TextWindow
 
 
 class Game():
@@ -13,66 +12,83 @@ class Game():
         self.game_display = pygame.display.set_mode((700, 700))
         pygame.display.set_caption("Connect four")
         self.running = True
-        self.clock = pygame.time.Clock()
-        self.click = False
+        # self.clock = pygame.time.Clock()
 
     def new_game(self):
-        self.turn = "AGENT"
-        self.running = True
         while self.running:
             self.grid = Grid(6, 7, self)
             self.agent = Agent(self.grid, self)
             self.run()
-            print("Game over\n")
-            self.reset = input("Restart (y/n) ? :\n")
-            if self.reset == "n":
+            if self.restart():
+                self.running = True
+            else:
                 self.running = False
+            # self.reset = input("Restart (y/n) ? :\n")
+            # if self.reset == "n":
+            #     self.running = False
 
     def run(self):
+        self.turn = "AGENT"
         self.playing = True
         while self.playing:
-            self.clock.tick(30)
+            self.check_filled()
+            # self.clock.tick(30)
             self.events()
+            pygame.time.wait(50)
             self.update()
             self.draw()
             if self.winning(self.grid.grid, 1) or self.winning(self.grid.grid, 2):
                 self.playing = False
 
     def events(self):
+        self.click = False
         for event in pygame.event.get():
-            if event.type == pygame.QUIT and self.playing:
+            if event.type == pygame.QUIT:
                 self.playing = False
                 self.running = False
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and self.playing:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 self.click = True
 
         self.keys = pygame.key.get_pressed()
         self.mouse_pos = pygame.mouse.get_pos()
 
-        if self.keys[pygame.K_ESCAPE] and self.playing:
+        if self.keys[pygame.K_ESCAPE]:
             self.playing = False
             self.running = False
             pygame.quit()
             sys.exit()
 
     def update(self):
-        if self.turn == "PLAYER":
-            if self.click:
-                self.grid.update(1)
-                self.click = False
-                self.turn = "AGENT"
-                pygame.time.wait(100)
-        elif self.turn == "AGENT":
-            pygame.time.wait(100)
+        if self.turn == "AGENT":
             self.grid.update(2)
             self.turn = "PLAYER"
+            pygame.time.wait(100)
+        elif self.turn == "PLAYER":
+            if self.click:
+                self.grid.update(1)
+                pygame.time.wait(100)
+                self.turn = "AGENT"
+                self.click = False
+
+        # elif self.turn == "AGENT":
+        #     self.grid.update(2)
+        #     self.turn = "PLAYER"
 
     def draw(self):
         self.game_display.fill((77, 86, 94))
         self.grid.draw_grid(self.game_display)
         pygame.display.update()
+
+    def check_filled(self):
+        self.filled_spots = 0
+        for columns in range(self.grid.cols):
+            if self.grid.open_spot(columns) == "XD":
+                self.filled_spots += 1
+
+        if self.filled_spots == self.grid.cols:
+            self.playing = False
 
     def winning(self, grid, piece):
 
@@ -109,7 +125,40 @@ class Game():
         return False
 
     def start_screen(self):
-        pass
+        self.button1 = Button(
+            700//2 - 140//2, 500, 140, 100, (255, 255, 255), (200, 200, 255), "START!", 40)
+        self.text_window = TextWindow(50, 100, 600, 200,
+                                      (255, 0, 255), "Connect four game", 80)
+        self.player_choose = False
+        while not self.player_choose:
+            self.events()
+            if self.click:
+                if self.mouse_pos[0] in range(350, 490) and self.mouse_pos[1] in range(500, 600):
+                    self.player_choose = True
+                # self.click = False
+            self.game_display.fill((0, 0, 0))
+            self.button1.draw(self.game_display)
+            self.text_window.draw(self.game_display)
+            pygame.display.update()
+
+    def restart(self):
+        while True:
+            self.text_window = TextWindow(
+                50, 10, 600, 50, (255, 255, 255), "Restart?", 40)
+            self.button1 = Button(
+                200, 70, 100, 30, (255, 255, 255), (35, 35, 35), "YES", 20)
+            self.button2 = Button(
+                540, 70, 100, 30, (255, 255, 255), (35, 35, 35), "NO", 20)
+            self.events()
+            if self.click:
+                if self.mouse_pos[0] in range(200, 300) and self.mouse_pos[1] in range(70, 100):
+                    return True
+                elif self.mouse_pos[0] in range(540, 640) and self.mouse_pos[1] in range(70, 100):
+                    return False
+            self.text_window.draw(self.game_display)
+            self.button1.draw(self.game_display)
+            self.button2.draw(self.game_display)
+            pygame.display.flip()
 
     def game_over(self):
         pass
